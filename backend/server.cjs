496bea5f044aad9131910fef12f81fd9ea7ebcdb
Burak Koc <koc.emre@fh-swf.de>
2024-09-28 16:33:02 +0100

    Erstellung des Backends
    Erstellung des REST APIs für das Webscraping.

---
+++ backend/server.cjs
@@ -0,0 +1,40 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+app.use(cors());
+
+// Mensa Plan laden 
+app.get('/api/meals/:mensa', async (req, res) => {
+    try{
+        console.log('Request');
+        console.log(req.params.mensa);
+        const loc = req.params.mensa.toLowerCase(); // Holt den Mensanamen aus der URL
+        console.log(loc);
+        const url = `https://www.stwdo.de/mensa-cafes-und-catering/fh-suedwestfalen/${loc}`;
+        const response = await axios.get(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        const mealsTable = $('.meals').html();
+
+        if(mealsTable){
+            res.json( { table: mealsTable } );
+            console.log('Successfully send response!');
+        } else {
+            res.status(500).json( { err: 'Fehler! Daten wurden nicht gesendet!' } );
+            console.log('Daten nicht gesendet!');
+        }
+    } catch(err){
+        console.log('Fehler beim Laden der Daten.', err);
+    }
+})
+
+// weitere end points
+
+app.listen(PORT, () => {
+    console.log('Server läuft auf Port 3000.');
+})
