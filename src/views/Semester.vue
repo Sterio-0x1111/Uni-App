6166b931e60fd5eaf78bf1f36178d68fdfa7f961
Burak Koc <koc.emre@fh-swf.de>
2024-09-30 16:40:19 +0100

    ion-grid für Darstellung der Semesterzeiträume hinzugefügt

--- src/views/Semester.vue
+++ src/views/Semester.vue
@@ -1,11 +1,29 @@
 <template>
     <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Semesterzeiten</ion-title>
+            </ion-toolbar>
+        </ion-header>
         <ion-content>
-            
+            <ion-grid>
+                <ion-row>
+                    <ion-col><strong>Semester</strong></ion-col>
+                    <ion-col><strong>Zeitraum</strong>/ion-col>
+                </ion-row>
+                <ion-row v-for="sem in semesterPeriods" :key="sem.semester">
+                    <ion-col>sem.semester</ion-col>
+                    <ion-col>sem.period</ion-col>
+                </ion-row>
+            </ion-grid>
         </ion-content>
     </ion-page>
 </template>
 
 <script setup>
+    import onMounted from 'vue';
 
+    onMounted(async () => {
+        const semesterPeriods = await fetch('http://lpcalhost:3000/api/semester');
+    })
 </script>
