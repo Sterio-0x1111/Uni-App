1ef8e26a3eafe11ab8f8214e71146707fac03a84
Burak Koc <koc.emre@fh-swf.de>
2024-10-05 20:34:57 +0100

    VSC Startseite und Prüfungsseite erstellt
    Die Seiten wurden initial angelegt.

---
+++ src/views/VSC/Exams.vue
@@ -0,0 +1,31 @@
+<template>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Prüfungen</ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+
+        </ion-content>
+    </ion-page>
+  </body>
+  </html>
+</template>
+
+<script setup>
+import {IonPage, IonContent, IonHeader, IonTitle, IonToolbar } from 'ionic/vue';
+</script>
+
+<style scoped>
+
+</style>
