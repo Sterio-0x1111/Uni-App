9cca8d9f02150ed060a6f4f68236c97c8fe1255e
Burak Koc <koc.emre@fh-swf.de>
2024-10-17 16:11:29 +0100

    Ionic Modal für Prüfungsdetails
    Ein Popup / Overlay für die Anzeige von Prüfungsdetails wurde hinzugefügt. Diese Ansicht gibt Auskunft über weitere Informationen zu Prüfungen, welche in der eingeschränkten Übersicht weggelassen wurden.

---
+++ src/views/VSC/ScoreDetails.vue
@@ -0,0 +1,30 @@
+<template>
+    <ion-modal :is-open="isOpen">
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Details</ion-title>
+                <ion-buttons slot="end">
+                    <ion-button @click="closeModal">OK</ion-button>
+                </ion-buttons>
+            </ion-toolbar>
+        </ion-header>
+        
+        <ion-content>
+
+        </ion-content>
+    </ion-modal>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    isOpen: Boolean, 
+    data: Object
+})
+
+const closeModal = () => {
+    props.isOpen = false;
+}
+
+</script>
