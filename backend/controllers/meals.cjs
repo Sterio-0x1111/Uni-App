066bc31a450620610210186e9b522a4b2af4ddb4
Burak Koc <koc.emre@fh-swf.de>
2024-10-23 16:57:53 +0100

    Controller korrigiert
    Der zuvor geladene Controller wurde fälschlicherweise verwechsel. Jetzt ist der richtige Controller drin.

--- backend/controllers/meals.cjs
+++
@@ -1,9 +0,0 @@
-// routes/meals.cjs
-const express = require("express");
-const { getMeals, getDates } = require("../controllers/mealsController.cjs");
-const router = express.Router();
-
-router.get('/:loc', getDates);
-router.get("/:mensa/:date", getMeals);
-
-module.exports = router;
