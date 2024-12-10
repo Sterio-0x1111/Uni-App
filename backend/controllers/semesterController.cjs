c6bb377f959ffe91e439b6ee14994abebf2f0213
Burak Koc <koc.emre@fh-swf.de>
2024-12-10 15:45:19 +0100

    Update Semester Controller
    Es wurden Funktion hinzugefügt, die die fachbereichsspezifischen Termine laden. Diese befinden sich auf derselben Seite, wie die Semestertermine, weshalb die Funktion in derselben Datei definiert sind. Die bisherigen Funktionen parsen lediglich die Fachbereiche, die als HTML Tabelle vorliegen.

--- backend/controllers/semesterController.cjs
+++ backend/controllers/semesterController.cjs
@@ -90,7 +90,7 @@ const getDepartments = async (req, res) => {
 /**
  * Funktion zum Laden von fachbereichsspezifischen Terminen.
  */
-const getSpecificDates = async (req, res) => {
+const getDepartmentDates = async (req, res) => {
     try {
 
         const url = 'https://www.fh-swf.de/de/studierende/studienorganisation/vorlesungszeiten/vorlesungzeit.php';
@@ -102,4 +102,117 @@ const getSpecificDates = async (req, res) => {
     }
 }
 
-module.exports = { getSemesterDates, getFeedbackDates, getSemesterDates, getDepartments }
+/**
+ * Diese Funktion lädt die als Tabelle bereitgestellten fachbereichspezifischen Semestertermine.
+ */
+const getDepartmentDatesAsTable = async (req, res) => {
+    const department = 'FB Informatik und Naturwissenschaften';
+    try {
+        // department soll später als dynamischer parameter übergeben werden (aus frontend)
+        const url = 'https://www.fh-swf.de/de/studierende/studienorganisation/vorlesungszeiten/vorlesungzeit.php';
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+
+        // JSON für dynamische Bestimmung der Filter Methode
+        const departments = {
+            'FB Elektrotechnik und Informationstechnik':    filterDepartmentTables,
+            'FB Technische Betriebswirtschaft':             filterDepartmentTables,
+            'FB Informatik und Naturwissenschaften':        filterDepartmentTablesWithLinks,
+            'FB Maschinenbau: Präsenzstudium':              filterDepartmentTablesWithLinks,
+            'FB Maschinenbau: Verbundstudium ':             filterExternalDepartmentTables,
+            'FB Ingenieur- und Wirtschaftswissenschaften':  filterDepartmentTables
+        }
+
+        const filterMethod = departments[department];
+        const dates = $('table').has(`tr[data-filter="${department}"]`).html();
+        const tableData = filterMethod($, dates);
+        
+        res.status(200).json({ tableData });
+
+    } catch(error){
+        console.log(`Fehler beim Laden der Termine für den Fachbereich ${department}.`, error);
+    }
+}
+
+/**
+ * Funktion zum Filtern der fachbereichsspezifischen Tabellen.
+ */
+const filterDepartmentTables = ($, dates) => {
+    try {
+        const tableData = [];
+        $(dates).find('tbody tr').each((index, row) => {
+            const cells = $(row).find('td').map((i, cell) => $(cell).text()).get();
+            tableData.push(cells);
+        })
+        return tableData;
+
+    } catch(error){
+        console.log('Fehler beim Filtern von Tabellen.', error);
+        return null;
+    }
+}
+
+/**
+ * Funktion zum Filtern von fachbereichsspezifischen Tabellen mit Links auf weitere Seiten.
+ */
+const filterDepartmentTablesWithLinks = ($, dates) => {
+    try {
+        const baseURL = 'https://www.fh-swf.de';
+        const tableData = [];
+
+        $(dates).find('tr').each((index, row) => {
+            const name = $(row).text().trim();
+            const link = $(row).find('a').map((index, a) => {
+                return {
+                    name: $(row).text().trim(),
+                    url: baseURL + $(a).attr('href')
+                }
+            }).get(0);
+            tableData.push(link);
+        })
+        return tableData;
+
+    } catch(error){
+        console.log('Fehler beim Filtern der Tabellen mit Links.', error);
+    }
+}
+
+/**
+ * Funktion zum Filtern von Verweistabellen.
+ * 
+ * Die Funktion parst parst Tabellen,
+ * die selbst keine Auskunft über Termine geben,
+ * sondern mit Links auf Moodle verweisen (FB 6).
+ * 
+ * @param $
+ *  Cheerio Objekt, dass die zu parsende Quellseite enthält
+ * @param dates
+ *  Cheerio Objekt, dass die zu filternde Tabele enthält.
+ * 
+ * @return tableData
+ *  Array, dass JSON Objekte mit den gefilterten Elementen enthält.
+ *  
+ */
+const filterExternalDepartmentTables = ($, dates) => {
+    try {
+
+        const tableData = [];
+        $(dates).find('tr').each((index, row) => {
+            const title = $(row).find('strong').html();
+            const link = $(row).find('a').map((index, a) => {
+                return {
+                    title,
+                    course: $(a).find('span').text().trim(),
+                    url: $(a).attr('href')
+                }
+            }).get(0);
+            tableData.push(link);
+        });
+        return tableData;
+
+    } catch(error){
+        console.log('Fehler beim Laden der externen Tabellen.', error);
+    }
+}
+
+module.exports = { getSemesterDates, getFeedbackDates, getSemesterDates, getDepartments, getDepartmentDatesAsTable }
--- backend/routes/semester.cjs
+++ backend/routes/semester.cjs
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getSemesterDates, getFeedbackDates, getDepartments } = require('../controllers/semesterController.cjs');
+const { getSemesterDates, getFeedbackDates, getDepartments, getDepartmentDatesAsTable } = require('../controllers/semesterController.cjs');
 
 router.get('/dates', getSemesterDates);
 router.get('/feedback', getFeedbackDates);
 router.get('/departments', getDepartments);
+router.get('/departments/dates', getDepartmentDatesAsTable);
 
 module.exports = router;
