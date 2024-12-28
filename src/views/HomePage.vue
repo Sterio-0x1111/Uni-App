2c90be2af4edcd774cee2889f85e33d96d12f730
Burak Koc <koc.emre@fh-swf.de>
2024-12-28 14:51:33 +0100

    Update der Startseite: Button Anapssung
    Der Button auf der Startseite wurde überarbeitet und ähnelt jetzt optisch dem Design der Hochschule.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -10,7 +10,7 @@
         <h2>Willkommen!</h2>
         <p>Dies ist das neue Informationssystem für Studierende der FH SWF!</p>
         <p>Hier können Sie auf verschiedene Seiten zugreifen, wie zum Beispiel Speisepläne, Prüfungsergebnisse oder Terminübersichten.</p>
-        <ion-button router-link="/navigation">
+        <ion-button :size="12" :size-md="6" :size-lg="4" class="custom-button" router-link="/navigation">
             Zur Seitenübersicht!
         </ion-button>
       </div>
@@ -38,31 +38,40 @@ const menuTitle = ref('Startseite');
 .container p {
   font-size: 20px;
 }
-#container {
+
+.container .custom-button {
+  display: flex;
+  justify-content: center;
   text-align: center;
-  
+}
+
+.custom-button {
+  --background: transparent;
+  --color: #ffffff;
+  --padding-start: 20px;
+  --padding-end: 20px;
+  --height: 50px;
+  --border-width: 3px;
+  --border-style: solid;
+  --border-color: blue;
+  --border-radius: 50px;
+  position: relative;
+  margin-top: 100px;
+  font-size: 16px;
+}
+
+.custom-button:before {
+  content: '';
   position: absolute;
-  left: 10%;
-  right: 10%;
-  top: 20%;
-  transform: translateY(-50%);
+  inset: 0; /* Füllt den gesamten Button aus */
+  border-radius: inherit;
+  padding: 2px; /* Abstand für den Farbverlauf */
+  --background: linear-gradient(90deg, #d32f7e, #a2275e); /* Farbverlauf */
+  -webkit-mask: linear-gradient(#000 0 0) content-box, linear-gradient(#000 0 0);
+  mask: linear-gradient(#000 0 0) content-box, linear-gradient(#000 0 0);
+  mask-composite: exclude;
+  -webkit-mask-composite: xor;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
 
-#container p {
-  font-size: 24px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
