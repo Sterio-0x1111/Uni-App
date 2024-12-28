49cc96fce63c9e3871db30f675c47a77cbf78e0b
Burak Koc <koc.emre@fh-swf.de>
2024-12-28 15:51:43 +0100

    Update Semester Routen: Department Routen entfernt

--- backend/routes/semester.cjs
+++ backend/routes/semester.cjs
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getSemesterDates, getFeedbackDates, getDepartments, getDepartmentDatesAsTable, getDepartmentDatesAsText, filterDepartmentTablesByLink } = require('../controllers/semesterController.cjs');
+const { getSemesterDates, getFeedbackDates } = require('../controllers/semesterController.cjs');
 
 router.get('/dates', getSemesterDates);
 router.get('/feedback', getFeedbackDates);
-router.get('/departments', getDepartments);
-//router.get('/departments/dates/:dep', getDepartmentDatesAsTable);
-router.post('/departments/dates', getDepartmentDatesAsTable);
-router.get('/departments/text', getDepartmentDatesAsText);
-router.post('/departments/course', filterDepartmentTablesByLink);
 
 module.exports = router;
