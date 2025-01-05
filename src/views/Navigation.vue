fd8c9cbf3ff6edd2512eef05f6785a139a76f0ca
Burak Koc <koc.emre@fh-swf.de>
2025-01-05 16:31:49 +0100

    Update Navigation: CSS Klassen und Icon
    Die Navigationsseite hat nun angepasstere Buttons (Größe) und zeigt nun ein Schlüssel Icon neben jedem Button an, der Authentifizierung benötigt. Außerdem wurden einige CSS KLassen in die globale variables.css ausgelagert und sind nicht mehr im lokalen Stylesheet.

--- src/views/Navigation.vue
+++ src/views/Navigation.vue
@@ -8,6 +8,9 @@
       <ion-list>
         <ion-button class="custom-button" v-for="route in filteredRoutes" :key="route.id"  :router-link="route.path">
           {{ route.title }}
+          <ion-icon name="key" v-if="route.requiresAuth" slot="start">
+
+          </ion-icon>
         </ion-button>
       </ion-list>
     </ion-content>
@@ -16,7 +19,7 @@
 
 <script setup lang="ts">
 import { ref, onMounted, computed } from 'vue';
-import {IonPage, IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonButton, } from "@ionic/vue";
+import {IonPage, IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonButton, IonIcon } from "@ionic/vue";
 import { useRouter } from "vue-router";
 import ToolbarMenu from "./ToolbarMenu.vue";
 import { useAuthStore } from "@/stores/authStore";
@@ -26,12 +29,12 @@ const authStore = useAuthStore();
 const loginState = computed(() => authStore.isLoggedIn);
 
 const routes: Route[] = [
-  { id: 0, title: "Mensaplan", path: "/meals", requiresAuth: false },
-  { id: 1, title: "Semestertermine", path: "/semester", requiresAuth: false },
-  { id: 2, title: "Fachbereichstermine", path: "/departments", requiresAuth: false },
-  //{ id: 2, title: "Login", path: "/login", requiresAuth: false },
-  { id: 3, title: "Lagepläne", path: "/locations", requiresAuth: false },
-  { id: 4, title: "Meine Prüfungen", path: "/exams", requiresAuth: true },
+  { id: 0, title: "Mensaplan",            path: "/meals",       requiresAuth: false },
+  { id: 1, title: "Semestertermine",      path: "/semester",    requiresAuth: false },
+  { id: 2, title: "Fachbereichstermine",  path: "/departments", requiresAuth: false },
+  { id: 3, title: "Lagepläne",            path: "/locations",   requiresAuth: false },
+  { id: 4, title: "Meine Prüfungen",      path: "/exams",       requiresAuth: true },
+//{ id: 2, title: "Login", path: "/login", requiresAuth: false },
 ];
 
 const filteredRoutes = computed(() => {
@@ -56,42 +59,25 @@ interface Route {
   path: string;
   requiresAuth: boolean;
 }
+
+import { addIcons } from 'ionicons'; 
+import { key } from 'ionicons/icons'; 
+addIcons({ 
+  'key': key
+});
 </script>
 
 <style scoped>
-ion-button{
+.custom-button{
   display: blocK;
   margin-left: 10px;
   margin-right: 10px;
   margin-top: 10px;
+
   
 }
 
-.custom-button {
-  --background: transparent;
-  --color: #ffffff;
-  --padding-start: 20px;
-  --padding-end: 20px;
-  --height: 50px;
-  --border-width: 3px;
-  --border-style: solid;
-  --border-color: blue;
-  --border-radius: 50px;
-  position: relative;
-  margin-top: 20px;
-  font-size: 16px;
-}
-
-.custom-button:before {
-  content: '';
-  position: absolute;
-  inset: 0; /* Füllt den gesamten Button aus */
-  border-radius: inherit;
-  padding: 2px; /* Abstand für den Farbverlauf */
-  --background: linear-gradient(90deg, #d32f7e, #a2275e); /* Farbverlauf */
-  -webkit-mask: linear-gradient(#000 0 0) content-box, linear-gradient(#000 0 0);
-  mask: linear-gradient(#000 0 0) content-box, linear-gradient(#000 0 0);
-  mask-composite: exclude;
-  -webkit-mask-composite: xor;
+ion-icon {
+  
 }
 </style>
